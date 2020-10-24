d8bb797fb24e93412ae323bb65055de4e57d2444
Victoria Cabral <43216482+toQhom@users.noreply.github.com>
2020-10-24 13:49:55 -0400

    Created framework for main.cpp and scenes

---
+++ the_princess_died/the_princess_died/scenes.cpp
@@ -0,0 +1,17 @@
+#include "scenes.h"
+
+void run_opening();
+void run_farm();
+void run_pirate_ship();
+void run_horse_ride();
+void run_eels();
+void run_cliffs();
+void run_poison();
+void run_fire_swamp();
+void run_pit();
+void run_miracle_max();
+void run_gate();
+void run_castle();
+void run_stable();
+void run_finale();
+void run_closing();
---
+++ the_princess_died/the_princess_died/scenes.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+class Scene {
+public:
+	void run_opening();
+	void run_farm();
+	void run_pirate_ship();
+	void run_horse_ride();
+	void run_eels();
+	void run_cliffs();
+	void run_poison();
+	void run_fire_swamp();
+	void run_pit();
+	void run_miracle_max();
+	void run_gate();
+	void run_castle();
+	void run_stable();
+	void run_finale();
+	void run_closing();
+private:
+	string input;
+};
